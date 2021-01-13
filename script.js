6f056673368f79dbc0dad1b452562c2f6b390113
Anil Pak <71243066+anLpk@users.noreply.github.com>
2021-01-13 23:17:58 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-    <script>
-
-      const inventors = [
-        { first: "Albert", last: "Einstein", year: 1879, passed: 1955 },
-        { first: "Isaac", last: "Newton", year: 1643, passed: 1727 },
-        { first: "Galileo", last: "Galilei", year: 1564, passed: 1642 },
-        { first: "Marie", last: "Curie", year: 1867, passed: 1934 },
-        { first: "Johannes", last: "Kepler", year: 1571, passed: 1630 },
-        { first: "Nicolaus", last: "Copernicus", year: 1473, passed: 1543 },
-        { first: "Max", last: "Planck", year: 1858, passed: 1947 },
-        { first: "Katherine", last: "Blodgett", year: 1898, passed: 1979 },
-        { first: "Ada", last: "Lovelace", year: 1815, passed: 1852 },
-        { first: "Sarah E.", last: "Goode", year: 1855, passed: 1905 },
-        { first: "Lise", last: "Meitner", year: 1878, passed: 1968 },
-        { first: "Hanna", last: "Hammarström", year: 1829, passed: 1909 },
-      ];
+ const inventors = [
+     { first: "Albert", last: "Einstein", year: 1879, passed: 1955 },
+     { first: "Isaac", last: "Newton", year: 1643, passed: 1727 },
+     { first: "Galileo", last: "Galilei", year: 1564, passed: 1642 },
+     { first: "Marie", last: "Curie", year: 1867, passed: 1934 },
+     { first: "Johannes", last: "Kepler", year: 1571, passed: 1630 },
+     { first: "Nicolaus", last: "Copernicus", year: 1473, passed: 1543 },
+     { first: "Max", last: "Planck", year: 1858, passed: 1947 },
+     { first: "Katherine", last: "Blodgett", year: 1898, passed: 1979 },
+     { first: "Ada", last: "Lovelace", year: 1815, passed: 1852 },
+     { first: "Sarah E.", last: "Goode", year: 1855, passed: 1905 },
+     { first: "Lise", last: "Meitner", year: 1878, passed: 1968 },
+     { first: "Hanna", last: "Hammarström", year: 1829, passed: 1909 },
+   ];
 
       const people = [
         "Beck, Glenn",
@@ -144,4 +142,51 @@
         return obj;
       }, {});
       console.log(transportation);
-    </script>
+
+// ## Array Cardio Day 2
+
+      const people = [
+        { name: "Wes", year: 1988 },
+        { name: "Kait", year: 1986 },
+        { name: "Irv", year: 1970 },
+        { name: "Lux", year: 2015 },
+      ];
+
+      const comments = [
+        { text: "Love this!", id: 523423 },
+        { text: "Super good", id: 823423 },
+        { text: "You are the best", id: 2039842 },
+        { text: "Ramen is my fav food ever", id: 123523 },
+        { text: "Nice Nice Nice!", id: 542328 },
+      ];
+
+      // Some and Every Checks
+      // Array.prototype.some() // is at least one person 19 or older?
+      const isAdult = people.some(
+        (person) => new Date().getFullYear() - person.year >= 19
+      );
+      // console.log({ isAdult });
+      // Array.prototype.every() // is everyone 19 or older?
+      const allAdult = people.every(
+        (person) => new Date().getFullYear() - person.year >= 19
+      );
+      // console.log({ allAdult });
+
+      // Array.prototype.find()
+      // Find is like filter, but instead returns just the one you are looking for
+      // find the comment with the ID of 823423
+      const comment = comments.find((comment) => comment.id === 823423);
+      // console.log(comment);
+
+      // Array.prototype.findIndex()
+      // Find the comment with this ID
+      const index = comments.findIndex((comment) => comment.id === 823423);
+      // console.log(index);
+      // delete the comment with the ID of 823423
+      comments.splice(index, 1);
+
+      const newComments = [
+        ...comments.slice(0, index),
+        ...comments.slice(index + 1),
+      ];
+//       console.table(newComments);
